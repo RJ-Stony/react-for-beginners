f90fd2e28baeff32b09f02490ced5aacd3d0eb8d
노준석 <joshua052698@gmail.com>
2024-01-02 17:32:14 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selectedCoin, setSelectedCoin] = useState(null);
+
+  const onChange = (event) => {
+    const coinId = event.target.value;
+    const coinData = coins.find((coin) => coin.id === coinId);
+    setSelectedCoin(coinData);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onChange}>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.id}>
+                {coin.name} ({coin.symbol})
+              </option>
+            ))}
+          </select>
+          {selectedCoin && <h3>How much {selectedCoin.name} can I buy?</h3>}
+          {selectedCoin && (
+            <p>
+              {selectedCoin.name} ({selectedCoin.symbol}): $
+              {selectedCoin.quotes.USD.price} USD
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/toDos.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    // ...는 배열의 요소들을 가져올 수 있음.
+    // setToDos((currentArray) => [toDo, ...currentArray]);
+    // 입력한 순서대로 출력하기 위함.
+    setToDos((currentArray) => [...currentArray, toDo]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
