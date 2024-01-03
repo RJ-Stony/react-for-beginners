2442bfff5e53bebd8e89708ff41810e56b8e5ca6
노준석 <joshua052698@gmail.com>
2024-01-03 16:57:33 +0900

    Design: 홈페이지 UI 변경

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
+// 수정 후 github.io 업데이트 하려면 npm run deploy
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Detail />} />
+        <Route path={"/" || `${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route
+          path={"/movie/:id" || `${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
       </Routes>
     </Router>
   );
